7118d6075b5675433f5e4ac1c89de60cc1c56a52
耗子 <haozi@loli.email>
2022-02-14 00:19:45 +0800

    修复：提交缺失的文件

---
+++ README.md
@@ -0,0 +1,21 @@
+# ArkPress
+
+### ArkPress - 没有区块的 WordPress 发行版
+
+不喜欢区块？讨厌古腾堡？那么恭喜你，这个项目将会很适合你！
+
+### 最新版本下载：
+
+## 我们所做的
+- 从底层完全剔除了区块编辑器（包括其依赖文件）
+- 基于 WordPress 5.9 构建（相比 WordPress 4.9 拥有更优秀的兼容性）
+
+## 我们将做的
+- 长期维护更新，以保证兼容最新版本的 PHP 和 Database
+- 长期安全更新，仅同步 WordPress 的安全更新，不引入区块相关功能
+
+## 技术支持
+- Q群：[1046115671](https://jq.qq.com/?_wv=1027&k=ItDgZVD6)
+- 社区： [litepress.cn/forums](https://litepress.cn/forums)
+
+### ArkPress 基于 [WordPress](https://cn.wordpress.org/) 构建，由 [LitePress](https://litepress.cn/) 团队负责维护
--- wp-content/themes/twentytwenty/searchform.php
+++ wp-content/themes/twentytwenty/searchform.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package ArkPress
+ * @package WordPress
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
@@ -23,7 +23,7 @@ if ( empty( $twentytwenty_aria_label ) && ! empty( $args['label'] ) ) {
 	$twentytwenty_aria_label = 'aria-label="' . esc_attr( $args['label'] ) . '"';
 }
 ?>
-<form role="search" <?php echo $twentytwenty_aria_label; // phpcs:ignore ArkPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" <?php echo $twentytwenty_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="<?php echo esc_attr( $twentytwenty_unique_id ); ?>">
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'twentytwenty' ); // phpcs:ignore: ArkPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></span>
 		<input type="search" id="<?php echo esc_attr( $twentytwenty_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentytwenty' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
